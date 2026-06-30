d988f1b355a987ce9582125b118d485f464f5fcd
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-06-30 22:34:27 +0100

    added a LoggingConfiguration class

---
+++ Logging/Configuration/LoggingConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace TradingEngineServer.Logging.Configuration;
+
+public class LoggingConfiguration
+{
+ public TextLogger TextLoggerConfiguration { get; set; }   
+}
+
+
+public class TextLoggerConfiguration
+{
+    public string directory { get; set; }
+    public string fileName { get; set; }    
+    public string fileExtension { get; set; }
+}
