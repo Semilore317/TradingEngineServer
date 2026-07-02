206c4e370c824e03e8209c0cda9cb1d657a58e03
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-02 22:41:14 +0100

    feat: created LogInfo record class

---
+++ Logging/LogInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace TradingEngineServer.Logging;
+
+public class LogInfo(
+    LogLevel logLevel, 
+    string module, 
+    string message, 
+    DateTime now, 
+    int currentThreadManagedThreadId, 
+    string? currentThreadName)
+{
+    public record LogInformation(
+        LogLevel LogLevel, 
+        string Module, 
+        string Message, 
+        DateTime Now, 
+        int ThreadId);
+}
