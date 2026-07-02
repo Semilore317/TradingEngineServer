1d992cf05d2ecddc6810e83b3577b6c1cd6aa5ac
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-02 23:03:02 +0100

    fix: fixed record structure of LogInfo class

--- Logging/LogInfo.cs
+++ Logging/LogInfo.cs
@@ -1,17 +1,10 @@
 ﻿namespace TradingEngineServer.Logging;
 
-public class LogInfo(
-    LogLevel logLevel, 
-    string module, 
-    string message, 
-    DateTime now, 
-    int currentThreadManagedThreadId, 
-    string? currentThreadName)
-{
-    public record LogInformation(
-        LogLevel LogLevel, 
-        string Module, 
-        string Message, 
-        DateTime Now, 
-        int ThreadId);
-}
+public record LogInfo(
+    LogLevel LogLevel,
+    string Module,
+    string Message,
+    DateTime Now,
+    int ThreadId,
+    string ThreadName
+);
