2024a9456f5c7815414428f834485d4c2ebd7900
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-04 22:34:49 +0100

    feat: implemented Order class

---
+++ Orders/Order.cs
@@ -0,0 +1,34 @@
+﻿using TradingEngineServer.Orders;
+
+namespace TradingEngineServer.Orders;
+
+public class Order(long orderId, long securityId, string username, Side side, long price, uint initialQuantity) 
+    : OrderCore(orderId, securityId, username)
+{
+    public Side Side { get; } = side;
+    public long Price { get; } = price; // in cents
+    public uint InitialQuantity { get; } = initialQuantity;
+    
+    
+    
+    // currentQUantity initially matches initialQuantity
+    public uint CurrentQuantity { get; private set; } = initialQuantity;
+
+
+    public void DecrementQuantity(uint amount)
+    {
+        if (amount > CurrentQuantity)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount));
+        }
+        else
+        {
+            CurrentQuantity -= amount;
+        }
+    }
+    
+    public  void IncrementQuantity(uint amount)
+    {
+        CurrentQuantity += amount;
+    }
+}
