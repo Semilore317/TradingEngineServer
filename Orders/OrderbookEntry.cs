e49d6a759158fe016f0a8a4f52701b2b955fe928
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-05 19:30:35 +0100

    feat(orders): implement Limit stub and OrderbookEntry resting node

---
+++ Orders/OrderbookEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace TradingEngineServer.Orders;
+
+public class OrderbookEntry: Order
+{
+    // i'm avoiding primary constructors here because it's less dense without it in this scenario
+    public OrderbookEntry(Order order, Limit parentLimit) :
+        base(order.OrderId, order.SecurityId, order.Username, order.Side, order.Price, order.InitialQuantity)
+    {
+        ParentLimit = parentLimit;
+    }
+
+    public Limit ParentLimit { get;}
+    public OrderbookEntry? Next { get; set; }
+    public OrderbookEntry? Previous { get; set; }
+    
+}
