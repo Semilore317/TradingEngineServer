b27e801084a22bdbf6beb534424504b08540682a
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-06 23:03:48 +0100

    feat(orders): implement ModifyOrder and CancelOrder data models

---
+++ Orders/CancelOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace TradingEngineServer.Orders;
+
+public class CancelOrder(
+      long orderId,
+      long securityId,
+      string username
+   )
+   : OrderCore(orderId, securityId, username)
+{
+    
+}
