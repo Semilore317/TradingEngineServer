8692188897f4b4843f04667a5758aeacf5d20196
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-06 21:04:28 +0100

    feat(orders): implement BidLimitComparer and AskLimitComparer with safe null handling using guard clauses

---
+++ Orders/LimitComparer.cs
@@ -0,0 +1,43 @@
+﻿namespace TradingEngineServer.Orders;
+
+/// <summary>
+/// due to the nature of the way we're presenting the bids and asks
+/// asks: ascending order --> buyers want the least limit
+/// bids: descending order --> sellers want the highest limit
+/// we need to be able to clearly compare and sort them
+/// </summary>
+public class BidLimitComparer : IComparer<Limit>
+{
+    public static IComparer<Limit> Comparer { get; } = new BidLimitComparer();
+
+    public int Compare(Limit? x, Limit? y)
+    {
+        // early returns
+        if (x == null && y == null)
+            return 0;
+        if (x == null)
+            return -1;
+        if (y == null)
+            return 1;
+        
+        return y.Price.CompareTo(x.Price);
+    }
+}
+
+public class AskLimitComparer : IComparer<Limit>
+{
+    public static IComparer<Limit> Comparer { get; } = new AskLimitComparer();
+
+    public int Compare(Limit? x, Limit? y)
+    {
+        // early returns
+        if (x == null && y == null)
+            return 0;
+        if (x == null)
+            return -1;
+        if (y == null)
+            return 1;
+        
+        return x.Price.CompareTo(y.Price);
+    }
+}
