bf337434e117cc0cae7c41f3f4e5a651a14f907b
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-16 20:52:16 +0100

    fix: MatchResult constructor now correctly assigns fills parameter

--- MatchingEngine/MatchResult.cs
+++ MatchingEngine/MatchResult.cs
@@ -6,6 +6,6 @@ public class MatchResult(
     List<Fill> fills
 )
 {
-    public List<Fill> Fills { get; } = new();
+    public List<Fill> Fills { get; } = fills;
     public bool IsMatch => Fills.Any(); // returns true if fills is NOT empty
 }
