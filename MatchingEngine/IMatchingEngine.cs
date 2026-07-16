c008402ad6dad48f50a80922f42091480e871024
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-16 14:16:24 +0100

    feat: wrote the matching engine interface

---
+++ MatchingEngine/IMatchingEngine.cs
@@ -0,0 +1,13 @@
+﻿using Instruments;
+using TradingEngineServer.OrderBook;
+using TradingEngineServer.Orders;
+
+namespace MatchingEngine;
+
+public interface IMatchingEngine
+{
+   void AddOrderBook(Security instrument);
+   MatchResult AddOrder(Order order);
+   MatchResult ChangeOrders(ModifyOrder modifyOrder);
+   void RemoveOrder(CancelOrder cancelOrder);
+}
