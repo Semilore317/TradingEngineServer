6a4fa1703dbaa564db9decc6d96bdfcb46f5fa2d
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-19 19:59:12 +0100

    feat: add REST DTOs

---
+++ Valkyrie/Api/Dto/OrderAck.cs
@@ -0,0 +1,17 @@
+﻿using Valkyrie.Api.Dto;
+using Valkyrie.MatchingEngine;
+
+namespace Valkyrie.Api;
+
+public record OrderAck(long OrderId, bool Matched, IReadOnlyList<FillDto> Fills)
+{
+    public static OrderAck From(long id, MatchResult matchResult)
+    {
+        return new (id, 
+            matchResult.IsMatch,
+            matchResult.Fills.Select(
+                f => new FillDto(
+                    f.BidOrderId, f.AskOrderId, f.ExecutionPrice, f.FilledQuantity )).ToList()
+            );
+    }
+}
