f913e5ff32264a532fae09414611c7a68cae5619
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-19 11:23:02 +0100

    chore: convert host to ASP.NET Core

--- Valkyrie/Program.cs
+++ Valkyrie/Program.cs
@@ -1,15 +1,55 @@
-﻿// entry point
-
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Valkyrie.Core;
+using Valkyrie.Core.Configuration;
+using Valkyrie.Logging;
+using Valkyrie.Logging.Configuration;
+using Valkyrie.MatchingEngine;
+using Valkyrie.MatchingEngine.Algorithms;
+using Valkyrie.MatchingEngine.Configuration;
+using static System.AppContext;
 
 using var engine = ValkyrieHostBuilder.BuildValkyrie();
 
 ValkyrieServiceProvider.ServiceProvider = engine.Services;
 
 {
-    using var scope = ValkyrieServiceProvider.ServiceProvider.CreateScope();
+    var builder = WebApplication.CreateBuilder(
+        new WebApplicationOptions
+        {
+            Args = args,
+            ContentRootPath = BaseDirectory, // read appsettings.json from the bin output, like the old host
+        });
+
+    builder.Configuration
+        .SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
     
-    await engine.RunAsync().ConfigureAwait(false);
+    builder.Services.AddOptions();
+    builder.Services.Configure<ValkyrieConfiguration>(
+        builder.Configuration.GetSection(nameof(ValkyrieConfiguration)));
+    builder.Services.Configure<LoggingConfiguration>(
+        builder.Configuration.GetSection(nameof(LoggingConfiguration)));
+    builder.Services.Configure<MatchingEngineConfiguration>(
+        builder.Configuration.GetSection(nameof(MatchingEngineConfiguration)));
+
+    builder.Services.AddSingleton<ITextLogger, TextLogger>();
+    builder.Services.AddSingleton<IMatchingAlgorithm>(sp =>
+    {
+        var config = sp.GetRequiredService<IOptions<MatchingEngineConfiguration>>().Value;
+        return config.Algorithm switch
+        {
+            MatchingAlgorithmType.Fifo => Fifo.Instance,
+            MatchingAlgorithmType.ProRata => ProRata.Instance,
+            _ => throw new InvalidOperationException($"Algorithm '{config.Algorithm}' is not supported.")
+        };
+    });
+
+    builder.Services.AddSingleton<IMatchingEngine, MatchingEngine>();
+    builder.Services.AddHostedService<Valkyrie.Core.Valkyrie>(); // the background service... it still runs
+
+    
+    var app = builder.Build();
+    await app.RunAsync();
 }
