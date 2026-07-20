8af14028d8c10971bd7de2b2efa6e50c69743f0d
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-20 18:29:11 +0100

    feat: publish market data from Order gateway

--- Valkyrie/Api/OrderGateway.cs
+++ Valkyrie/Api/OrderGateway.cs
@@ -1,24 +1,46 @@
 using Valkyrie.Api.Dto;
+using Valkyrie.Api.MarketData;
 using Valkyrie.MatchingEngine;
 using Valkyrie.Orders;
 
 namespace Valkyrie.Api;
 
-public sealed class OrderGateway(IMatchingEngine engine)
+/// <summary>
+///  provides a thread-safe entry point for order operations routing to the matching engine
+///  It uses a private lock object pattern to guarantee a mutex across all state mutations and reads
+/// </summary>
+public sealed class OrderGateway(IMatchingEngine engine, IMarketDataPublisher publisher)
 {
     private readonly object _gate = new();
     private long _nextOrderId;
 
     public OrderAck Submit(PlaceOrderRequest request)
     {
-        lock (_gate)                                // one writer at a time
+
+        MatchResult result;
+        long id;
+        OrderBookSnapshot? snapshot;
+        
+        // syncs access to the sequential ID generation and the underlying non-thread-safe matching engine instance
+        // this prevents lock contention from external code
+        lock (_gate)                          
         {
             // server assigns the id for now.... since it's locked it's not a major concern for now
-            var id = ++_nextOrderId;          
-            var order = new Order(id, request.SecurityId, request.Username, request.Side, request.Price, request.Quantity);
-            var result = engine.AddOrder(order);
-            return OrderAck.From(id, result);
+            id = ++_nextOrderId;          
+            result = engine.AddOrder(
+                new Order(
+                    id, request.SecurityId, request.Username, request.Side, request.Price, request.Quantity));
+            engine.TryGetSnapshot(request.SecurityId, out snapshot);
         }
+
+        foreach (var fill in result.Fills)
+            publisher.PublishTrade(TradeEvent.From(fill));
+        
+        if(snapshot != null)
+            publisher.PublishBook(snapshot);
+
+
+        return OrderAck.From(id, result);
     }
 
     public void Cancel(long id, long securityId, string username)
