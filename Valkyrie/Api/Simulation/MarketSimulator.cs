e9d04d1eeb0f6d16971c6561671795fc87fcfc6a
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-07-22 12:38:29 +0100

    fix: moved Synthetic market source to it's proper directory to match it's namespace and added gates back into market simulator

--- Valkyrie/Api/Simulation/MarketSimulator.cs
+++ Valkyrie/Api/Simulation/MarketSimulator.cs
@@ -1,4 +1,6 @@
-﻿using Valkyrie.Logging;
+﻿using Microsoft.Extensions.Options;
+using Valkyrie.Core.Configuration;
+using Valkyrie.Logging;
 
 namespace Valkyrie.Api.Simulation;
 
@@ -8,11 +10,17 @@ namespace Valkyrie.Api.Simulation;
 /// </summary>
 public class MarketSimulator(
     IMarketDataSource source,
-    ITextLogger logger
+    ITextLogger logger,
+    IOptions<MarketSimulatorConfiguration> config
 ) : BackgroundService
 {
     protected override Task ExecuteAsync(CancellationToken token)
     {
+        if (!config.Value.Enabled)
+        {
+            logger.Info("MarketSimulator", "disabled");
+            return Task.CompletedTask;
+        }
         logger.Info("MarketSimulatorRunner", $"Starting Data Source: {source.Name}");
         return source.RunAsync(token);
     }
